34b5f96285c9787f6e89be8711d15d890c6d0254
RedSkotina <red.skotina@gmail.com>
2017-06-12 23:32:19 +0300

    start implementation

--- samples/injector/injector.cpp
+++ samples/injector/injector.cpp
@@ -4,40 +4,29 @@
 
 #include <easyhook.h>
 
-int _tmain(int argc, _TCHAR* argv[])
+int wmain( int argc, wchar_t *argv[ ], wchar_t *envp[ ] )  
 {
-	
-	WCHAR* InEXEPath = L"..\\Debug\\target.exe";
+	ULONG OutProcessId = 0;
+	WCHAR* InEXEPath = L"..\\..\\target\\build\\target.exe";
 	WCHAR* InCommandLine = L"";
-	WCHAR* dllToInject = L"..\\inject.dll";
+	WCHAR* dllToInject = L".\\inject.dll";
 	wprintf(L"Attempting to inject: %s\n\n", dllToInject);
-
-	// Inject dllToInject into the target process Id, passing 
-	// freqOffset as the pass through data.
-	NTSTATUS nt = RhInjectLibrary(
-		processId,   // The process to inject into
-		0,           // ThreadId to wake up upon injection
-		EASYHOOK_INJECT_DEFAULT,
-		dllToInject, // 32-bit
-		NULL,		 // 64-bit not provided
-		&freqOffset, // data to send to injected DLL entry point
-		sizeof(DWORD)// size of data to send
-	);
     
-    EASYHOOK_NT_EXPORT exp =  RhCreateAndInject(
+    RhCreateAndInject(
         InEXEPath,
         InCommandLine,
+        0,
         EASYHOOK_INJECT_DEFAULT,
         dllToInject,
         NULL,
-        PVOID InPassThruBuffer,
-        ULONG InPassThruSize,
-        ULONG* OutProcessId); 
+        NULL,
+        0,
+        &OutProcessId); 
 
 
-	if (nt != 0)
+	if (OutProcessId == 0 )
 	{
-		printf("RhInjectLibrary failed with error code = %d\n", nt);
+		//printf("RhCreateAndInject failed with error code = %d\n", nt);
 		PWCHAR err = RtlGetLastErrorString();
 		std::wcout << err << "\n";
 	}
